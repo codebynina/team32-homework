c167bdeb49f6f30e1d0bc28a46c005974c9362dd
niña <ninamarieondevilla@gmail.com>
2025-02-04 15:19:28 +0100

    Dog Age Calculator

---
+++ javascript/javascript1/week1/dogAgeCalculator.js
@@ -0,0 +1,15 @@
+const dogYearOfBirth = 2022;
+const dogYearFuture = 2045;
+const shouldShowResultInDogYears = true;
+
+const humanAge = dogYearFuture - dogYearOfBirth;
+
+if (shouldShowResultInDogYears) {
+dogYear = humanAge *7 }
+else { dogYear = humanAge;}
+
+if (shouldShowResultInDogYears) {
+console.log ("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);}
+
+else {console.log ("Your dog will be " + dogYear + " human years old in " + dogYearFuture);}
+
