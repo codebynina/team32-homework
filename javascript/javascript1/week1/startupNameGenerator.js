eabc27c30d1b7c8ee6999e9b034874639cc89307
niña <ninamarieondevilla@gmail.com>
2025-02-10 12:18:02 +0100

    Updated based on mentor feedback

--- javascript/javascript1/week1/startupNameGenerator.js
+++ javascript/javascript1/week1/startupNameGenerator.js
@@ -1,11 +1,11 @@
-let firstWords = ["Tech", "Creative", "Bright", "Smart", "Future", "Innovator", "Dynamic", "Artistic", "Witty", "Labs", "Geometric", "Solution", "Boost", "Spark", "Empower",]
+let firstWords = ["Creative", "Bright", "Innovator", "Dynamic", "Witty", "Labs", "Geometric", "Solution", "Boost", "Spark"];
 
-let secondWords = ["Clean", "Elegant", "Adventurous", "Curious", "Polish", "Cozy", "Curated", "Floral", "Engaging", "Quirky", "AI", "Eco", "Digital",]
+let secondWords = ["Clean", "Elegant", "Adventurous", "Curious", "Cozy", "Curated", "Floral", "Engaging","AI", "Eco"];
 
-const randomNumber1 = Math.floor(Math.random() *13);
-const randomNumber2 = Math.floor(Math.random() *13);
+const randomNumber1 = Math.floor(Math.random() *10);
+const randomNumber2 = Math.floor(Math.random() *10);
 
-let startupName = firstWords[randomNumber1] + " " + secondWords[randomNumber2];
+const startupName = `${firstWords[randomNumber1]}${secondWords[randomNumber2]}`;
 
 
-console.log("The startup: " + startupName + " contains " + startupName.length + "characters.");
+console.log(`The startup: ${startupName} contains ${startupName.length} characters.`);
