3d7f7cbd39c47d726590852ec3ed067f6bc68ac7
niña <ninamarieondevilla@gmail.com>
2025-02-10 12:08:40 +0100

    Updated based on mentor feedback

--- javascript/javascript1/week1/ageCalculator.js
+++ javascript/javascript1/week1/ageCalculator.js
@@ -5,4 +5,4 @@ const yearFuture  = 2045;
 
 const age = yearFuture - yearOfBirth;
 
-console.log("You will be " + age + " years old in " +  yearFuture);
+console.log(`You will be ${age} years old in ${yearFuture}`);
