5f4f540d7348e61e947008a859a9871d02b81ed5
niña <ninamarieondevilla@gmail.com>
2025-02-10 12:13:22 +0100

    Updated based on mentor feedback

--- javascript/javascript1/week1/dogAgeCalculator.js
+++ javascript/javascript1/week1/dogAgeCalculator.js
@@ -3,13 +3,7 @@ const dogYearFuture = 2045;
 const shouldShowResultInDogYears = true;
 
 const humanAge = dogYearFuture - dogYearOfBirth;
+const dogYear = shouldShowResultInDogYears ? humanAge * 7 : humanAge;
 
-if (shouldShowResultInDogYears) {
-dogYear = humanAge *7 }
-else { dogYear = humanAge;}
 
-if (shouldShowResultInDogYears) {
-console.log ("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);}
-
-else {console.log ("Your dog will be " + dogYear + " human years old in " + dogYearFuture);}
-
+console.log (`Your dog will be ${dogYear} ${shouldShowResultInDogYears ? "dog" : "human"} years old in ${dogYearFuture}.`);
