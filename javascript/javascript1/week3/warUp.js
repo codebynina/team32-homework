939453a10ddbbc42e5f62857230eba72cd41e300
niña <ninamarieondevilla@gmail.com>
2025-02-19 23:15:35 +0100

    javasSript1-week3 (warm-up)

---
+++ javascript/javascript1/week3/warUp.js
@@ -0,0 +1,15 @@
+
+const names = [
+    "Peter",
+    "Ahamad",
+    "Yana",
+    "Rasmus",
+    "Samuel",
+    "Katrine",
+    "Tala",
+];
+const nameToRemove = "Ahamad";
+delete names[1];
+
+console.log(names);
+
